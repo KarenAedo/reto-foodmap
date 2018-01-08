729aac90286af3f31c88c8aa905bc220a91c7475
KarenAedo <karen.aedo.mayorga@gmail.com>
2018-01-08 01:36:10 -0300

    Cambios de estilos e incorporación de data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
 	
-	//Splash
-   	setTimeout(function(){
-   		$('#splash').fadeOut(500);
-   	}, 2000);
-   
-   	//input filtro
-   	var elements = $('article')
-   	$('select').change(function(){
-   		var value = $('option:selected').text().toLowerCase();
-   		console.log(value);
-   		$('article').detach();
-   		elements.appendTo('restaurantes');
-   		if(value != 'selecione') $('article').show().filter(':not(.'+ value +')').detach();
-   	});
+//SPLASH - función para que aparezca el splash
+//setTimeout función para el tiempo
+setTimeout(function(){
+   $('#splash').fadeOut(500); //fadeOut, para desaparecer cosas
+  }, 2000);
 
-   //mouseover'
+}); //document.ready
 
 
 
-}); //.ready
+/*for(var i = 0; i < restaurants.length; i++){
+  var createRestaurants = $('#restaurants').append(
+    '<div class="row character">' +
+    '<div class= "col-md-6 text-center">' +
+    '<img src="assets/' + restaurants[i].photo + '">' +
+    '</div>' +
+    '<div class= "col-md-6">'+
+    '<h3>' + restaurants[i].name + '</h3>' +
+    '<p>School: ' + restaurants[i].location + '</p>' +
+    '<p>City: ' + restaurants[i].foodType + '</p>' +
+    '</div>' +
+    '</div>');
+}
+
+$(#filter).on('change', function(){
+  var inputValue = $(#filter).val();
+  $('.food-type').each(function(index, value){
+    var temp = $(this).text();
+      if(temp == inputValue){
+        $(this).parent().parent().parent().parent().show();
+      } else {
+        $(this).parent().parent().parent().parent().hide();
+      }
+  });
+}); */  
---
+++ assets/js/data.js
@@ -0,0 +1,49 @@
+var restaurants = [
+	{'name': 'Sazón Nazca',
+	 'location': 'Edwards 636',
+	 'foodType': 'Comida peruana',
+	 'photo': 'assets/images/aji-de-gallina.jpg'
+	},
+	
+	{'name': 'Peruchos'
+	 'location': 'Esmeralda 1151',
+	 'foodType': 'Comida peruana',
+	 'photo': 'assets/images/pollada-peruana.jpg',
+	},
+	
+	{'name': 'Wang Lung',
+	 'location': 'Almirante Barroso 517',
+	 'foodType': 'Comida china',
+	 'photo': 'assets/images/pollo-estilo-oriental.jpg',
+	},
+	
+	{'name': 'Chui Yin',
+	 'location': 'Pedro Montt 2059',
+	 'foodType': 'Comida china',
+	 'photo':
+	},
+
+	{'name': 'La Mangiata'
+	 'location': 'Rodrígues 538',
+	 'foodType': 'Comida italiana',
+	 'photo':
+	},
+
+	{'name': 'Il Paparazzo',
+	 'location': 'Papudo 424'
+	 'foodType': 'Comida italiana',
+	 'photo':
+	},
+
+	{'name': 'Caleta Portales Restaurant',
+	 'location': 'Acceso a Caleta Portales',
+	 'foodType': 'Pescados y mariscos',
+	 'photo':
+	},
+
+	{'name': 'Porto Viejo',
+	 'location': 'Cochrane 99',
+	 'foodType': 'Pescados y mariscos',
+	 'photo':
+	},	
+]
